e36fd1fc5b527646feffe1dc9f3b36a2332b525a
Dennis Muiruri <muirurid@puhti-login11.bullx>
2023-03-30 12:37:10 +0300

    Added comments on how to use the count_lines script

--- count_lines.sh
+++ count_lines.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# This script is used to evaluate the number of items in a given class
+# as listed in the annotations folder. The annotations folder
+# container a file named by class and then file contains the id of the
+# file.
+
+# This script should be used as follows: ./count_lines.sh annotations
+
 # Check if the directory argument is supplied
 if [ $# -eq 0 ]
 then
